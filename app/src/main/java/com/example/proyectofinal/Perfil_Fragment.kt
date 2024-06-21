1559d2cc040cbb6beb73e5d7d79a35576e547a2b
JorLOrT <jortizc@ulasalle.edu.pe>
2024-06-21 02:45:50 -0500

    Creación del proyecto, creación de la navegación y del primer fragmento como placeholder.

---
+++ app/src/main/java/com/example/proyectofinal/Perfil_Fragment.kt
@@ -0,0 +1,12 @@
+package com.example.proyectofinal
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+class PerfilFragment : Fragment(R.layout.fragment_perfil_) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
