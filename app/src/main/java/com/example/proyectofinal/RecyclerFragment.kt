3ef1b28b766710c618b2be1edb3290bf12e7fb83
JorLOrT <jortizc@ulasalle.edu.pe>
2024-06-30 14:50:59 -0500

    Creacion del RecyclerFragment.

---
+++ app/src/main/java/com/example/proyectofinal/RecyclerFragment.kt
@@ -0,0 +1,12 @@
+package com.example.proyectofinal
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+
+class RecyclerFragment : Fragment(R.layout.fragment_recycler) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
