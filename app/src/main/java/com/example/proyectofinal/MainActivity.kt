8433e5762e3a436ee8c9599e5fdebdf305454c3d
JorLOrT <jortizc@ulasalle.edu.pe>
2024-06-30 14:46:35 -0500

    Creación del bottom_navigation_menu e implementación de la navegación entre pestañas (botones).

--- app/src/main/java/com/example/proyectofinal/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinal/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.proyectofinal
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navFragment = supportFragmentManager.findFragmentById(R.id.contenedor_fragment) as NavHostFragment
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navFragment.navController)
+
     }
 }
