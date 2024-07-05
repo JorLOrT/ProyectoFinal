d8c3c6d231dbd6be30037dfb8a50810a0e230f21
JorLOrT <jortizc@ulasalle.edu.pe>
2024-07-04 22:24:34 -0500

    Se mejoró la legibilidad del código y se añadieron mensajes de error personalizados para el Login_Fragment y para el SignUp_Fragment.

--- app/src/main/java/com/example/proyectofinal/Login_Fragment.kt
+++ app/src/main/java/com/example/proyectofinal/Login_Fragment.kt
@@ -10,63 +10,54 @@ import android.widget.TextView
 import androidx.core.os.bundleOf
 import androidx.navigation.findNavController
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 class Login_Fragment : Fragment(R.layout.fragment_login_) {
 
     lateinit var input_email: EditText
     lateinit var input_password: EditText
     lateinit var btn_login: Button
+    lateinit var btn_signup: TextView
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         input_email = view.findViewById<EditText>(R.id.input_email)
         input_password = view.findViewById<EditText>(R.id.input_password)
         btn_login = view.findViewById<Button>(R.id.logear_cuenta)
-        val btn_signup = view.findViewById<TextView>(R.id.texto_registrarse)
-        btn_signup.setOnClickListener {
-            irSignUp()
-        }
+        btn_signup = view.findViewById<TextView>(R.id.texto_registrarse)
+        irSignUp()
         logearUsuario()
     }
 
     fun logearUsuario(){
-
         btn_login.setOnClickListener {
-            if(input_email.text.isNotEmpty() && input_password.text.isNotEmpty()){
-                FirebaseAuth.getInstance().signInWithEmailAndPassword(input_email.text.toString(), input_password.text.toString()).addOnCompleteListener {
+            if(input_email.text.isEmpty() || input_password.text.isEmpty()){
+                mostrarAlerta("Error", "Debe completar todos los campos")
+                return@setOnClickListener
+            }
+            else{
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(input_email.text.toString(), input_password.text.toString()).addOnCompleteListener {
                     if(it.isSuccessful){
                         cambiarVista(it.result?.user?.email ?: "", ProviderType.BASIC)
                     }else{
-                        mostrarAlerta()
+                        mostrarAlerta("Error", "Error al logearse")
                     }
                 }
-            }else{
-                alertaCampos()
             }
-
         }
     }
 
-    fun mostrarAlerta(){
+    fun mostrarAlerta(tipo: String, mensaje: String){
         val msj = AlertDialog.Builder(this.context)
-        msj.setTitle("Error")
-        msj.setMessage("Usuario o Password incorrectos")
-        msj.setPositiveButton("Aceptar", null)
-        msj.show()
-    }
-
-    fun alertaCampos(){
-        val msj = AlertDialog.Builder(this.context)
-        msj.setTitle("Error")
-        msj.setMessage("Debe llenar todos los campos")
+        msj.setTitle(tipo)
+        msj.setMessage(mensaje)
         msj.setPositiveButton("Aceptar", null)
         msj.show()
     }
 
     private fun irSignUp(){
-        view?.findNavController()?.navigate(R.id.action_login_Fragment_to_signUp_Fragment)
+        btn_signup.setOnClickListener {
+            view?.findNavController()?.navigate(R.id.action_login_Fragment_to_signUp_Fragment)
+        }
     }
 
     private fun cambiarVista(email: String, provider: ProviderType){
--- app/src/main/java/com/example/proyectofinal/SignUp_Fragment.kt
+++ app/src/main/java/com/example/proyectofinal/SignUp_Fragment.kt
@@ -17,47 +17,53 @@ class SignUp_Fragment : Fragment(R.layout.fragment_sign_up_) {
     lateinit var input_email: EditText
     lateinit var input_password: EditText
     lateinit var input_confirm_password: EditText
+    lateinit var btn_login: TextView
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         btn_signup = view.findViewById(R.id.crear_cuenta)
         input_email = view.findViewById(R.id.input_email_signup)
         input_password = view.findViewById(R.id.input_password_signup1)
         input_confirm_password = view.findViewById(R.id.input_password_signup2)
-        val btn_login = view.findViewById<TextView>(R.id.texto_iniciar_sesion)
-        btn_login.setOnClickListener {
-            irLogin()
-        }
+        btn_login = view.findViewById<TextView>(R.id.texto_iniciar_sesion)
 
+        irLogin()
         registrarUsuario()
     }
 
     fun registrarUsuario(){
-
         btn_signup.setOnClickListener {
-            if(input_email.text.isNotEmpty() && input_password.text.isNotEmpty() && input_confirm_password.text.toString() == input_password.text.toString()){
+            if(input_email.text.isEmpty() || input_password.text.isEmpty() || input_confirm_password.text.isEmpty()){
+                mostrarAlerta("Error", "Debe completar todos los campos")
+                return@setOnClickListener
+            }
+            else if(input_password.text.toString() != input_confirm_password.text.toString() ){
+                mostrarAlerta("Error", "Las passwords no coinciden")
+                return@setOnClickListener
+            }
+            else{
                 FirebaseAuth.getInstance().createUserWithEmailAndPassword(input_email.text.toString(), input_password.text.toString()).addOnCompleteListener {
                     if(it.isSuccessful){
                         cambiarVista(it.result?.user?.email ?: "", ProviderType.BASIC)
                     }else{
-                        mostrarAlerta()
+                        mostrarAlerta("Error", "Error al registrarse")
                     }
                 }
-            }else{
-                mostrarAlerta()
             }
         }
     }
 
-    fun mostrarAlerta(){
+    fun mostrarAlerta(tipo: String, mensaje: String){
         val msj = AlertDialog.Builder(this.context)
-        msj.setTitle("Error")
-        msj.setMessage("Error al registrarse")
+        msj.setTitle(tipo)
+        msj.setMessage(mensaje)
         msj.setPositiveButton("Aceptar", null)
         msj.show()
     }
 
     private fun irLogin(){
-        view?.findNavController()?.navigate(R.id.action_signUp_Fragment_to_login_Fragment)
+        btn_login.setOnClickListener {
+            view?.findNavController()?.navigate(R.id.action_signUp_Fragment_to_login_Fragment)
+        }
     }
 
     private fun cambiarVista(email: String, provider: ProviderType){
